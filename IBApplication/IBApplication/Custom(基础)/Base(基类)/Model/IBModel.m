3742edff65d5cb8a8069eef03466a7871228d95f
BowenCoder <tc_bowen@sina.com>
2018-07-09 19:25:15 +0800

    增加基类声明

---
+++ IBApplication/IBApplication/Custom(基础)/Base(基类)/Model/IBModel.m
@@ -0,0 +1,13 @@
+//
+//  IBModel.m
+//  IBApplication
+//
+//  Created by Bowen on 2018/7/9.
+//  Copyright © 2018年 BowenCoder. All rights reserved.
+//
+
+#import "IBModel.h"
+
+@implementation IBModel
+
+@end
---
+++ IBApplication/IBApplication/Custom(基础)/Base(基类)/View/IBView.m
@@ -0,0 +1,21 @@
+//
+//  IBView.m
+//  IBApplication
+//
+//  Created by Bowen on 2018/7/9.
+//  Copyright © 2018年 BowenCoder. All rights reserved.
+//
+
+#import "IBView.h"
+
+@implementation IBView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
