d35ff98c78d2e12158078622794a9fa9e04db7fd
bowenzheng <bowenzheng@inke.cn>
2018-09-14 11:20:43 +0800

    测试

--- IBApplication/IBApplication/Modules(模块)/Home/Controller/ViewController.m
+++ IBApplication/IBApplication/Modules(模块)/Home/Controller/ViewController.m
@@ -23,6 +23,7 @@
 #import "IBShareManager.h"
 #import "IBAuthManager.h"
 #import "IBActionSheet.h"
+#import "MBProgressHUD+Ext.h"
 
 @interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
 
@@ -55,32 +56,8 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [self.view endEditing:YES];
-    IBShareObject *obj = [[IBShareObject alloc] init];
-    obj.platformType = IBSharePlatformWechatTimeLine;
-    obj.image = [UIImage imageNamed:@"test"];
-    obj.previewImage = [UIImage imageNamed:@"bg"];
-    obj.urlString = @"www.baidu.com";
-//    obj.previewUrlString = @"www.inke.cn";
-    obj.title = @"123456789";
-    obj.text = @"qwert";
-//    [[IBShareManager manager] shareImage:obj success:^(id response) {
-//
-//    } failure:^(NSError *error) {
-//
-//    }];
-//    [[IBShareManager manager] shareLink:obj success:^(id response) {
-//
-//    } failure:^(NSError *error) {
-//
-//    }];
-    
-    IBActionItem *item = [IBActionItem itemWithType:IBActionTypeNormal image:[UIImage imageNamed:@"FSActionSheet_cancel"] title:@"你好" tintColor:[UIColor redColor] handler:^(IBActionItem *item, NSInteger selectedIndex) {
-        NSLog(@"123");
-    }];
-    IBActionSheet *sheet = [[IBActionSheet alloc] initWithTitle:@"2018年7月3日，曾经为08年总冠军凯尔特人立下汗马功劳的拉简-朗多宣布以1年900万签约湖人，他的决定从某种程度上比勒布朗西游洛杉矶还要让人惊讶：这位绿军夺冠功臣、半年前还怒喷小托马斯配不上凯尔特人的老派球员，居然加入了一支曾在总决赛中相互纠缠怒目相对的队伍，更不要提两支球队自上世纪60年代延续至今的恩怨情仇，以及他个人和勒布朗之间的宿怨。" cancelItem:nil items:@[item, item, item, item, item, item, item, item, item, item, item, item]];
-    [sheet show];
 
-    
+//    [MBProgressHUD showCircleLoadingView:self.view];
 //    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
 //    view.backgroundColor = [UIColor redColor];
 //    self.popupManager = [IBPopupManager popupManagerWithMaskType:IBPopupMaskTypeBlackBlur];
