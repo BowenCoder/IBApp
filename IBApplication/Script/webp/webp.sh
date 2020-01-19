a23136564726d9b8964df601edd00a8acd920ceb
正北飞 <tc_bowen@sina.com>
2020-01-20 02:51:45 +0800

    libwenp

--- IBApplication/Script/webp/webp.sh
+++ IBApplication/Script/webp/webp.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #指定libwebP的版本号
-version=${1:-1.0.0}
+version=${1:-1.1.0}
 #cocoaPods master中的git地址
 oldSource="https://chromium.googlesource.com/webm/libwebp"
 #替换为Github上的新地址
 newSource="https://github.com/webmproject/libwebp.git"
 
-echo -e '\033[36mFinding the path, please wait for a moment... \033[0m'
+echo -e 'Finding the version('$version') path, please wait for a moment...'
 
-path=$(find ~/.cocoapods/repos/master -iname libwebp)
+path=$(find ~/.cocoapods/repos/master/Specs/1/9/2/ -iname libwebp)
 path=$path"/"$version"/libwebp.podspec.json"
 
 if [ ! -f $path ];then
-	echo -e '\033[31mError:libwebp.podspec does not exist. Please check the path:'$path '\033[0m'
+	echo -e 'Error:libwebp.podspec does not exist. Please check the path:'$path ''
 else 
 	oldSource=${oldSource//\//\\/}
 	newSource=${newSource//\//\\/}
 	sed -i '' 's/'$oldSource'/'$newSource'/g' $path
-	echo -e '\033[32mOPERATION SUCCESS. \033[0m'
+	echo -e 'OPERATION SUCCESS.'
 fi
 
 
