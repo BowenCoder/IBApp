c650328aa969f394956132bde61523796390172e
正北飞 <1214569257@qq.com>
2020-03-31 12:59:34 +0800

    MBPopupController

--- IBApplication/IBApplication/Custom(基础)/Public(公共)/MBButton/MBButton.m
+++ IBApplication/IBApplication/Custom(基础)/Public(公共)/MBButton/MBButton.m
@@ -7,14 +7,43 @@
 //
 
 #import "MBButton.h"
+#import "CALayer+Ext.h"
 
 @interface MBButton ()
 
 @property (nonatomic, assign) NSTimeInterval acceptEventTime;
+@property (nonatomic, strong) CALayer *highlightedBackgroundLayer;
+@property (nonatomic, strong) UIColor *originBorderColor;
 
 @end
 
 @implementation MBButton
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setupView];
+    }
+    return self;
+}
+
+- (void)setupView
+{
+    self.adjustsTitleTintColorAutomatically = NO;
+    self.adjustsImageTintColorAutomatically = NO;
+    
+    // 默认接管highlighted和disabled的表现，去掉系统默认的表现
+    self.adjustsImageWhenHighlighted = NO;
+    self.adjustsImageWhenDisabled = NO;
+    self.adjustsButtonWhenHighlighted = YES;
+    self.adjustsButtonWhenDisabled = YES;
+    
+    if (!self.adjustsTitleTintColorAutomatically) {
+        [self setTitleColor:self.tintColor forState:UIControlStateNormal];
+    }
+    
+}
 
 - (void)sendAction:(SEL)action to:(id)target forEvent:(UIEvent *)event
 {
@@ -78,6 +107,149 @@
     
     self.titleEdgeInsets = labelEdgeInsets;
     self.imageEdgeInsets = imageEdgeInsets;
+}
+
+#pragma mark - setter
+
+- (void)setHighlightedBackgroundColor:(UIColor *)highlightedBackgroundColor {
+    _highlightedBackgroundColor = highlightedBackgroundColor;
+    if (_highlightedBackgroundColor) {
+        // 只要开启了highlightedBackgroundColor，就默认不需要alpha的高亮
+        self.adjustsButtonWhenHighlighted = NO;
+    }
+}
+
+- (void)setHighlightedBorderColor:(UIColor *)highlightedBorderColor {
+    _highlightedBorderColor = highlightedBorderColor;
+    if (_highlightedBorderColor) {
+        // 只要开启了highlightedBorderColor，就默认不需要alpha的高亮
+        self.adjustsButtonWhenHighlighted = NO;
+    }
+}
+
+- (void)setHighlighted:(BOOL)highlighted {
+    [super setHighlighted:highlighted];
+    
+    if (highlighted && !self.originBorderColor) {
+        // 手指按在按钮上会不断触发setHighlighted:，所以这里做了保护，设置过一次就不用再设置了
+        self.originBorderColor = [UIColor colorWithCGColor:self.layer.borderColor];
+    }
+    
+    // 渲染背景色
+    if (self.highlightedBackgroundColor || self.highlightedBorderColor) {
+        [self adjustsButtonHighlighted];
+    }
+    // 如果此时是disabled，则disabled的样式优先
+    if (!self.enabled) {
+        return;
+    }
+    // 自定义highlighted样式
+    if (self.adjustsButtonWhenHighlighted) {
+        if (highlighted) {
+            self.alpha = 0.8;
+        } else {
+            self.alpha = 1;
+        }
+    }
+}
+
+- (void)setEnabled:(BOOL)enabled {
+    [super setEnabled:enabled];
+    if (!enabled && self.adjustsButtonWhenDisabled) {
+        self.alpha = 0.8;
+    } else {
+        self.alpha = 1;
+    }
+}
+
+- (void)adjustsButtonHighlighted {
+    if (self.highlightedBackgroundColor) {
+        if (!self.highlightedBackgroundLayer) {
+            self.highlightedBackgroundLayer = [CALayer layer];
+            [self.highlightedBackgroundLayer fb_removeDefaultAnimations];
+            [self.layer insertSublayer:self.highlightedBackgroundLayer atIndex:0];
+        }
+        self.highlightedBackgroundLayer.frame = self.bounds;
+        self.highlightedBackgroundLayer.cornerRadius = self.layer.cornerRadius;
+        self.highlightedBackgroundLayer.backgroundColor = self.highlighted ? self.highlightedBackgroundColor.CGColor : [UIColor clearColor].CGColor;
+    }
+    
+    if (self.highlightedBorderColor) {
+        self.layer.borderColor = self.highlighted ? self.highlightedBorderColor.CGColor : self.originBorderColor.CGColor;
+    }
+}
+
+- (void)setAdjustsTitleTintColorAutomatically:(BOOL)adjustsTitleTintColorAutomatically {
+    _adjustsTitleTintColorAutomatically = adjustsTitleTintColorAutomatically;
+    [self updateTitleColorIfNeeded];
+}
+
+- (void)updateTitleColorIfNeeded {
+    if (self.adjustsTitleTintColorAutomatically && self.currentTitleColor) {
+        [self setTitleColor:self.tintColor forState:UIControlStateNormal];
+    }
+    if (self.adjustsTitleTintColorAutomatically && self.currentAttributedTitle) {
+        NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithAttributedString:self.currentAttributedTitle];
+        [attributedString addAttribute:NSForegroundColorAttributeName value:self.tintColor range:NSMakeRange(0, attributedString.length)];
+        [self setAttributedTitle:attributedString forState:UIControlStateNormal];
+    }
+}
+
+- (void)setAdjustsImageTintColorAutomatically:(BOOL)adjustsImageTintColorAutomatically {
+    BOOL valueDifference = _adjustsImageTintColorAutomatically != adjustsImageTintColorAutomatically;
+    _adjustsImageTintColorAutomatically = adjustsImageTintColorAutomatically;
+    
+    if (valueDifference) {
+        [self updateImageRenderingModeIfNeeded];
+    }
+}
+
+- (void)updateImageRenderingModeIfNeeded {
+    if (self.currentImage) {
+        NSArray<NSNumber *> *states = @[@(UIControlStateNormal), @(UIControlStateHighlighted), @(UIControlStateSelected), @(UIControlStateSelected|UIControlStateHighlighted), @(UIControlStateDisabled)];
+        
+        for (NSNumber *number in states) {
+            UIImage *image = [self imageForState:number.unsignedIntegerValue];
+            if (!image) {
+                return;
+            }
+            
+            if (self.adjustsImageTintColorAutomatically) {
+                // 这里的 setImage: 操作不需要使用 renderingMode 对 image 重新处理，而是放到重写的 setImage:forState 里去做就行了
+                [self setImage:image forState:[number unsignedIntegerValue]];
+            } else {
+                // 如果不需要用template的模式渲染，并且之前是使用template的，则把renderingMode改回Original
+                [self setImage:[image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] forState:[number unsignedIntegerValue]];
+            }
+        }
+    }
+}
+
+- (void)setImage:(UIImage *)image forState:(UIControlState)state {
+    if (self.adjustsImageTintColorAutomatically) {
+        image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    }
+    
+    [super setImage:image forState:state];
+}
+
+- (void)tintColorDidChange {
+    [super tintColorDidChange];
+    
+    [self updateTitleColorIfNeeded];
+    
+    if (self.adjustsImageTintColorAutomatically) {
+        [self updateImageRenderingModeIfNeeded];
+    }
+}
+
+- (void)setTintColorAdjustsTitleAndImage:(UIColor *)tintColorAdjustsTitleAndImage {
+    _tintColorAdjustsTitleAndImage = tintColorAdjustsTitleAndImage;
+    if (tintColorAdjustsTitleAndImage) {
+        self.tintColor = tintColorAdjustsTitleAndImage;
+        self.adjustsTitleTintColorAutomatically = YES;
+        self.adjustsImageTintColorAutomatically = YES;
+    }
 }
 
 @end
